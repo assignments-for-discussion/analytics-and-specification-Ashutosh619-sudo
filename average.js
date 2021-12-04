318458cb0086b4c28cb154e9c72b59a791fd0d44
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2021-12-04 22:11:13 +0530

    update average.js
    added code to deal with NaN and empty lists

--- average.js
+++ average.js
@@ -1,6 +1,22 @@
-
-function average(numbers) {
-  return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+function average(numbers){
+    var avg = 0;
+    if(numbers.length == 0){
+        return NaN;
+    }
+    else{
+        var sum = 0
+        var noOfNumberNan = 0
+        numbers.forEach(function(number){
+            if(!isNaN(number)){
+                sum += number;
+            }
+            else{
+                noOfNumberNan += 1
+            }
+        });
+        avg = sum / (numbers.length-noOfNumberNan);
+    }
+    return avg;
 }
 
 module.exports = {average};
