076b23deed38e57e74123e2e0230981a3a101593
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2021-12-07 13:22:28 +0530

    update average.js

--- average.js
+++ average.js
@@ -1,8 +1,4 @@
 function average(numbers){
-    if(numbers.length == 0){
-        return NaN;
-    }
-
     var result = numbers.filter(function(number){
         if(isNaN(number) || number < -50 || number > 80){
             return false;
