354691c48ea663a90f6aeec5e4aff87126c25e61
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2021-12-07 12:35:51 +0530

    update average.js

--- average.js
+++ average.js
@@ -1,24 +1,28 @@
 function average(numbers){
-    var avg = 0;
     if(numbers.length == 0){
         return NaN;
     }
-    else{
-        var sum = 0
-        // variable to check number of nan values 
-        var noOfNaN= 0
-        numbers.forEach(function(number){
-            if(!isNaN(number)){
-                sum += number;
-            }
-            else{
-                noOfNaN += 1
-            }
-        });
-        /* if nan values are present, 
-           then we need to subtract them from the length of the array */
-        avg = sum / (numbers.length-noOfNaN); 
-    }
-    return avg;
+
+    var result = numbers.filter(function(number){
+        if(isNaN(number)){
+            return false;
+        }
+        // outliers below -50 and above 80
+        if(number < -50 || number > 80){
+            return false;
+        }
+
+        return true;
+
+    });
+
+    sum = 0
+    result.forEach(function(number){
+        sum += number;
+    });
+
+    return sum / result.length;
+
 }
+
 module.exports = {average};
