d54250514a033903b2d0f4931e55f2e32ba34116
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2021-12-08 11:59:28 +0530

    update average.js

--- average.js
+++ average.js
@@ -1,21 +1,47 @@
+function findFilterOutliers(numbers) { 
+
+    var values = numbers.concat();
+
+    values.sort( function(a, b) {
+            return a - b;
+         });
+
+
+    var quartile1 = values[Math.floor((values.length / 4))];
+    var quartile3 = values[Math.ceil(values.length * (3 / 4))-1];
+    var interQaurtileRange = quartile3 - quartile1;
+
+    var maxValue = quartile3 + interQaurtileRange*1.5;
+    var minValue = quartile1 - interQaurtileRange*1.5;
+
+    var filteredValues = values.filter(function(x) {
+        return (x <= maxValue) && (x >= minValue);
+    });
+
+
+    return filteredValues;
+}
+
 function average(numbers){
     if(numbers.length == 0){
         return NaN;
     }
 
     var result = numbers.filter(function(number){
-        if(!isNaN(number) && (number > -50 && number < 80)){
+        if(!isNaN(number)){
             return true;
         }
-
     });
 
+    var filtered = findFilterOutliers(result);
+
     var sum = 0
-    result.forEach(function(number){
+    filtered.forEach(function(number){
         sum += number;
     });
 
-    return sum / result.length;
+
+    return sum / filtered.length;
 
 }
 
